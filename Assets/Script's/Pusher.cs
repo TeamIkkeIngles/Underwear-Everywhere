6b99ca3bbeb797916b600facfe4b22498620bf82
Johan44445 <karstein.haarsaker@gmail.com>
2025-03-04 12:14:07 +0100

    I fineshed the map and made some player movment

---
+++ Assets/Script's/Pusher.cs
@@ -0,0 +1,22 @@
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class Pusher : MonoBehaviour
+{
+    Allways allways;
+    public int PushUpPower = 10;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        allways = (Allways)FindAnyObjectByType(typeof(Allways));
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            allways.pushup(PushUpPower);
+            Debug.Log("triggered");
+        }
+    }
+}
