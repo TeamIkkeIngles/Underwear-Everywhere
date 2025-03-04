d3be95c06b333f939ce0a8e760c67d7a37b1f702
ProgrammerDuck2 <43863123+Iverander@users.noreply.github.com>
2025-03-04 23:23:41 +0100

    pretty good now

--- Assets/!Scripts/Player/PlayerMovement.cs
+++ Assets/!Scripts/Player/PlayerMovement.cs
@@ -12,9 +12,9 @@ public class PlayerMovement : MonoBehaviour
     [Header("References")]
     [NaughtyAttributes.ReadOnly, SerializeField] private float currentSpeed;
     Vector3 movementValue;
-    private float TiltValue, LerpValue;
+    float tiltValue, lerpValue;
 
-    private Transform CameraTransform;
+    Transform cameraTransform;
 
     [Header("Settings")]
     [SerializeField] Cloth cloth;
@@ -22,19 +22,22 @@ public class PlayerMovement : MonoBehaviour
     [Space]
 
     [SerializeField] float movementSpeed = 30;
-    [SerializeField] private MinMax thrustSpeed;
-    [SerializeField] private float ThrustMultiplier;
-    [SerializeField] private float DragFactor;
-    [SerializeField] private float MinDrag;
-    [SerializeField] private float RotationSpeed;
-    [SerializeField] private float TiltStrength = 90;
-    [SerializeField] private float LowPercent = 0.1f, HighPercent = 1;
+    [SerializeField] MinMax thrustSpeed;
+    [SerializeField] float ThrustMultiplier;
+    [SerializeField] float DragFactor;
+    [SerializeField] float MinDrag;
+    [SerializeField] float RotationSpeed;
+    [SerializeField] float TiltStrength = 90;
+    [SerializeField] float LowPercent = 0.1f, HighPercent = 1;
 
     private void Start()
     {
         Cursor.lockState = CursorLockMode.Locked;
 
-        CameraTransform = Camera.main.transform;
+        cameraTransform = Camera.main.transform;
+
+        currentSpeed = thrustSpeed.max;
+        rb.AddRelativeForce(Vector3.forward * currentSpeed, ForceMode.Impulse);
     }
 
     private void Update()
@@ -52,10 +55,11 @@ public class PlayerMovement : MonoBehaviour
         float pitchInRads = transform.eulerAngles.x * Mathf.Rad2Deg;
         float mappedPitch = -Mathf.Sin(pitchInRads) * ThrustMultiplier;
         float offsetMappedPitch = Mathf.Cos(pitchInRads) * DragFactor;
+        float acceleration = transform.eulerAngles.x >= 300? LowPercent : HighPercent;
 
         Vector3 speed = Vector3.forward * currentSpeed;
 
-        currentSpeed += mappedPitch * Time.fixedDeltaTime;
+        currentSpeed += mappedPitch * acceleration * Time.fixedDeltaTime;
         currentSpeed = Mathf.Clamp(currentSpeed, 0, thrustSpeed.max);
 
         cloth.externalAcceleration = -rb.linearVelocity;
@@ -78,18 +82,19 @@ public class PlayerMovement : MonoBehaviour
 
     private void ManageRotation()
     {
-        //TiltValue += mouseX * TiltStrength * Time.deltaTime;
+        tiltValue += -mouseX * TiltStrength * Time.deltaTime;
 
-        //if (mouseX == 0)
-        //{
-        //    TiltValue = Mathf.Lerp(TiltValue, 0, LerpValue);
-        //    LerpValue += Time.deltaTime;
-        //} else
-        //{
-        //    LerpValue = 0;
-        //}
+        if (mouseX == 0)
+        {
+            tiltValue = Mathf.Lerp(tiltValue, 0, lerpValue);
+            lerpValue += Time.deltaTime;
+        }
+        else
+        {
+            lerpValue = 0;
+        }
 
-        Quaternion targetRotation = Quaternion.Euler(CameraTransform.eulerAngles.x, CameraTransform.eulerAngles.y, TiltValue);
+        Quaternion targetRotation = Quaternion.Euler(cameraTransform.eulerAngles.x, cameraTransform.eulerAngles.y, tiltValue);
         transform.rotation = Quaternion.Lerp(transform.rotation, targetRotation, RotationSpeed * Time.deltaTime);
     }
 }
